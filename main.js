409ed16f88a37660439d8b46c7ba106e6470163c
Beneatha Hayes <you@example.com>
2024-02-28 10:42:48 -0600

    completed

---
+++ main.js
@@ -0,0 +1,132 @@
+// Task 1 Research
+/**What are the Primitive data types?
+ * this is my answer to the question above
+ * string
+ * number
+ * boolean
+ * undefined
+ * symbol
+ * null
+ */
+
+/**What are the Reference(Complex) data types we discussed in class?
+ * Here's a list of reference data types for the question above.
+ *Array
+ *Object
+ *Function
+*/
+
+/**What is the Spread Operator?
+ *The spread operator (...) allows us to quickly copy all or part of an existing array or object.
+ */
+
+/**Research the Math object in Javascript and tell me three of the methods you find and their use.
+ *Here are three important methods from the Math object
+ * Math.abs(x)' - returns the absolute value of number, which is the value of the number without regard to its sign.. It takes one argument, x, which can be any number.
+ * Math.random(): This method returns a floating-point, pseudo-random number that is greater than or equal to  0 but less than 1, with approximately uniform distribution.
+ * Math.PI': This property represents the value of the mathematical constant pi, which is approximately equal to 3.1. It is often used in mathematics when dealing with circles or trigonometry.
+ * /
+ 
+/**Research the Array Methods in Javascript and tell me three of the methods you find and their use.
+ *These are three important array methods I found:
+ * pop( ) - removes last item from array and returns it 
+ * push(item) - Adds one or many items to the end of the array and returns the new length of the array
+ * shift() - Removes first item from array and returns it
+ */
+
+/** Research the String Methods in Javascript and tell me three of the methods you find and their use.
+ *Here are the important string methods that I found.
+ * toUpperCase() Method: This method is used to convert all the characters in a string to uppercase letters. It does not modify the original string but returns a new string with all characters converted to uppercase.
+ * substring() Method: The substring() method is used to extract a portion of a string and return it as a new       string. It takes two parameters: the starting index (inclusive) and the ending index (exclusive) of the substring.
+ * indexOf() Method: The indexOf() method is used to find the index of the first occurrence of a specified value within a string. If the value is not found, it returns -1.
+ */
+
+//Does a string have indexes?
+/**Yes, a string does have indexes. 
+ * questions continued...
+*/
+
+//How do we access different items in an Array?
+/**We can access different items in an array using their index numbers. The index starts at zero.
+ * question  continued..
+*/
+
+//how do we access different properties in an Object? 
+/**To access different properties in an object, we use dot notation. We also use bracket notation which allows us to use variables as property names
+ * question  continued ...
+*/
+
+//how do you check a strings length?
+/**To get the length of a string , you would use the `length` property. This property returns the number of characters in a string. 
+ * question  continued ...
+*/
+
+/**how do you check an arrays length?
+ *You can check the length of an array using the length property. The length property returns the number of elements in an array.
+ * question   continued ...
+ */
+ 
+//Task 2 -
+/**Initialize a variable called firstName with your first name
+ *let firstName = "Beneatha";
+ * question continued ..
+ */
+
+/**Initialize a variable called lastName with your last name
+/* let lastName = "Hayes"
+ * question continued..
+ */
+
+/**Initialize a variable called lovesToCode with a boolean value
+ * let lovesToCode = true;
+ * question continued..
+ */
+
+//Task 3 - Creating a function
+/**Create a function that is given the first and last name then returns the fullname of the person
+ *function getFullName(firstName, lastName) {
+  return firstName + " " + lastName;
+}
+ * question  continued..
+ */
+
+/**Initialize the returned value of the function to a variable called fullName.
+ *let fullName = getFullName(firstName,lastName);
+ * question continued... 
+ */
+
+ //Task 4 - Objects and Arrays
+ /**Initialize a `book` object and give it the following three properties: title, author, publishedYear. The book information you will have to look up is `Ready Player One`
+ * let book = {
+    title: "Ready Player One",
+    author: "Ernest Cline",
+    publishedYear: 2011
+    };
+ * question continued...  
+ */
+
+ /** Initialize a `movie` object and give it the following three properties: title, director, releaseYear. The movie information you will have to look up is `Inception`
+ * let movie= {
+ *   title:  "Inception",
+ *   director: "Christopher Nolan",
+ *   yearReleased: 2010
+ * };
+ * question continued...
+ */  
+
+ /**Initialize a `student` object and give it the following three properties: name, age, grades. The student information is your self. The `grades` property should be an array of the following 3 number grades: 85, 90, 92.
+ * 
+ const student = {
+  name: "Beneatha",
+  age: "Infinite",
+  grades: [85, 90, 92]
+ };
+ * continues..
+ */
+
+
+
+
+
+
+
